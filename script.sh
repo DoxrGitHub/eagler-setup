487463dbc8c5485bcd2b391a5ed83c3887a98159
DoxrGitHub <132623420+DoxrGitHub@users.noreply.github.com>
2024-08-21 15:22:05 -0500

    main script

--- script.sh
+++ script.sh
@@ -1,1 +1,29 @@
-echo "yo sigma"
+echo '[EAGSCRIPT] grabbing Eaglercraft server + bungee'
+git clone https://git.eaglercraft.rip/eaglercraft/eaglercraft-1.8-replit.git
+mv eaglercraft-1.8-replit eaglercraftx
+cd eaglercraftx
+
+# delete server stuff
+  rm -rf server/world
+  rm -rf server/world_nether
+  rm -rf server/world_the_end
+  rm -rf server/logs
+  rm -rf server/plugins/PluginMetrics
+  rm -f server/usercache.json
+
+# mod bungee
+  rm -rf bungee/logs
+  rm -f bungee/eaglercraft_skins_cache.db
+  rm -f bungee/eaglercraft_auths.db
+  sed -i '/^stats: /d' bungee/config.yml
+  sed -i "s/^server_uuid: .*\$/server_uuid: $(cat /proc/sys/kernel/random/uuid)/" bungee/plugins/EaglercraftXBungee/settings.yml
+
+echo '[EAGSCRIPT] grabbing the latest EaglerXBungee build'
+curl -s https://git.eaglercraft.rip/eaglercraft/eaglercraft-builds/src/branch/main/EaglercraftX_1.8_EaglerXBungee.jar -o bungee/plugins/eaglerxbungee.jar 
+
+echo '[EAGSCRIPT] compressing the server/proxy build...'
+cd ..
+zip build.eaglerx.zip eaglercraftx
+
+echo '[EAGSCRIPT] compressing the server/proxy build...'
+echo '[internal] completed'
